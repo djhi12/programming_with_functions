257e1ab43b55bfdb25e97cdba7676a71fb348b4b
Daniel John Idorot <ido20002@byui.edu>
2023-06-05 20:33:25 +0800

    Initial commit

--- week1/tire_volume.py
+++ week1/tire_volume.py
@@ -1,33 +1,21 @@
-import math
+from datetime import date
 
-"""
-v is the volume in liters,
-π is the constant PI, which is the ratio of the circumference of a circle divided by its diameter (use math.pi),
-w is the width of the tire in millimeters,
-a is the aspect ratio of the tire, and
-d is the diameter of the wheel in inches.
+def calculate_volume(width, aspect_ratio, diameter):
+    volume = (3.14159 * width**2 * aspect_ratio * (width * aspect_ratio + 2540 * diameter)) / 10000000000
+    return round(volume, 2)
 
-Formula: v = pi * w**2 * a (w * a + 2540 * d) / 10000000000
+def main():
+    width = int(input("Enter the width of the tire in mm (ex 205): "))
+    aspect_ratio = int(input("Enter the aspect ratio of the tire (ex 60): "))
+    diameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
 
-> python tire_volume.py
-Enter the width of the tire in mm (ex 205): 185
-Enter the aspect ratio of the tire (ex 60): 50
-Enter the diameter of the wheel in inches (ex 15): 14
-The approximate volume is 24.09 liters
+    volume = calculate_volume(width, aspect_ratio, diameter)
+    print("The approximate volume is", volume, "liters")
 
-> python tire_volume.py
-Enter the width of the tire in mm (ex 205): 205
-Enter the aspect ratio of the tire (ex 60): 60
-Enter the diameter of the wheel in inches (ex 15): 15
-The approximate volume is 39.92 liters
-"""
+    # Append volume data to the file
+    with open("volumes.txt", "a") as file:
+        today = date.today().strftime("%Y-%m-%d")
+        print(f"{today}, {width}, {aspect_ratio}, {diameter}, {volume}\n")
 
-tire_width = float(input("\nEnter the width of the tire in mm (ex 205): "))
-aspect_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
-diameter = float(input("Enter the diameter of the wheel in inches (ex 15): "))
-
-# Formula: v = pi * w**2 * a (w * a + 2540 * d) / 10000000000
-volume = math.pi * tire_width ** 2 * aspect_ratio * (tire_width * aspect_ratio + 2540 * diameter) / 10000000000
-
-print(f"\nThe approximate volume in {round(volume, 2)} liters.\n")
-
+# Call the main function
+main()
