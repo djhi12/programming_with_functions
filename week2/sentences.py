7cd30bdf0f52d82a84fe1d3bbfa16dbdc3fc9ddf
Daniel John Idorot <ido20002@byui.edu>
2023-06-14 20:06:49 +0800

    Initial commit

---
+++ week2/sentences.py
@@ -0,0 +1,45 @@
+import random
+
+def main():
+    pass
+
+
+def make_sentence():
+    pass
+
+
+def get_determiner(quantity):
+    """Return a randomly chosen determiner. A determiner is
+    a word like "the", "a", "one", "some", "many".
+    If quantity is 1, this function will return either "a",
+    "one", or "the". Otherwise this function will return
+    either "some", "many", or "the".
+
+    Parameter
+        quantity: an integer.
+            If quantity is 1, this function will return a
+            determiner for a single noun. Otherwise this
+            function will return a determiner for a plural
+            noun.
+    Return: a randomly chosen determiner.
+    """
+    if quantity == 1:
+        words = ["a", "one", "the"]
+    else:
+        words = ["some", "many", "the"]
+
+    # Randomly choose and return a determiner.
+    word = random.choice(words)
+    return word
+
+
+def get_noun():
+    pass
+
+
+def get_verb():
+    pass
+
+
+# Call the main function
+main()
