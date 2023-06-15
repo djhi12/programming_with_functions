7caf66662bfe9bd389277a6a8c3f60b55f5a0387
Daniel John Idorot <ido20002@byui.edu>
2023-06-15 13:11:17 +0800

    Initial commit

--- week2/sentences.py
+++ week2/sentences.py
@@ -1,45 +1,61 @@
 import random
 
 def main():
-    pass
+    words = ["boy", "girl", "cat", "dog", "bird", "house"]
+    word = random.choice(words)
+    cap_word = word.capitalize()
+    # print(cap_word)
+
+    quantity = random.randint(1, 3)  # Random quantity: 1, 2, or 3
+    tense = random.choice(["past", "present", "future"])  # Random tense: past, present, or future
+
+    sentence = make_sentence(quantity, tense)
+    print(sentence.capitalize())
 
 
-def make_sentence():
-    pass
+def make_sentence(quantity, tense):
+    determiner = get_determiner(quantity)
+    noun = get_noun(quantity)
+    verb = get_verb(quantity, tense)
+
+    sentence = f"{determiner} {noun} {verb}."
+    return sentence
 
 
 def get_determiner(quantity):
-    """Return a randomly chosen determiner. A determiner is
-    a word like "the", "a", "one", "some", "many".
-    If quantity is 1, this function will return either "a",
-    "one", or "the". Otherwise this function will return
-    either "some", "many", or "the".
-
-    Parameter
-        quantity: an integer.
-            If quantity is 1, this function will return a
-            determiner for a single noun. Otherwise this
-            function will return a determiner for a plural
-            noun.
-    Return: a randomly chosen determiner.
-    """
     if quantity == 1:
-        words = ["a", "one", "the"]
+        determiners = ["a", "one", "the"]
     else:
-        words = ["some", "many", "the"]
+        determiners = ["some", "many", "the"]
 
-    # Randomly choose and return a determiner.
-    word = random.choice(words)
-    return word
-
-
-def get_noun():
-    pass
+    determiner = random.choice(determiners)
+    return determiner
 
 
-def get_verb():
-    pass
+def get_noun(quantity):
+    if quantity == 1:
+        nouns = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+    else:
+        nouns = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+
+    noun = random.choice(nouns)
+    return noun
 
 
-# Call the main function
+def get_verb(quantity, tense):
+    if tense == "past":
+        verbs = ["drank", "ate", "grew", "laughed", "thought", "ran", "slept", "talked", "walked", "wrote"]
+    elif tense == "present" and quantity == 1:
+        verbs = ["drinks", "eats", "grows", "laughs", "thinks", "runs", "sleeps", "talks", "walks", "writes"]
+    elif tense == "present" and quantity != 1:
+        verbs = ["drink", "eat", "grow", "laugh", "think", "run", "sleep", "talk", "walk", "write"]
+    elif tense == "future":
+        verbs = ["will drink", "will eat", "will grow", "will laugh", "will think", "will run", "will sleep", "will talk", "will walk", "will write"]
+    else:
+        print("Type the correct tense.")
+        return None
+
+    verb = random.choice(verbs)
+    return verb
+
 main()
