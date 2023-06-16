fb36303ee43dbfc7b93f446482bff55a1def7c15
Daniel John Idorot <ido20002@byui.edu>
2023-06-16 10:33:38 +0800

    Initial commit

--- week2/sentences.py
+++ week2/sentences.py
@@ -1,16 +1,19 @@
 import random
 
+
 def main():
     words = ["boy", "girl", "cat", "dog", "bird", "house"]
     word = random.choice(words)
     cap_word = word.capitalize()
     # print(cap_word)
 
-    quantity = random.randint(1, 3)  # Random quantity: 1, 2, or 3
-    tense = random.choice(["past", "present", "future"])  # Random tense: past, present, or future
+    for _ in range(6):  # Call make_sentence six times
+        quantity = random.randint(1, 3)  # Random quantity: 1, 2, or 3
+        # Random tense: past, present, or future
+        tense = random.choice(["past", "present", "future"])
 
-    sentence = make_sentence(quantity, tense)
-    print(sentence.capitalize())
+        sentence = make_sentence(quantity, tense)
+        print(sentence.capitalize())
 
 
 def make_sentence(quantity, tense):
@@ -34,9 +37,11 @@ def get_determiner(quantity):
 
 def get_noun(quantity):
     if quantity == 1:
-        nouns = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+        nouns = ["bird", "boy", "car", "cat", "child",
+                 "dog", "girl", "man", "rabbit", "woman"]
     else:
-        nouns = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+        nouns = ["birds", "boys", "cars", "cats", "children",
+                 "dogs", "girls", "men", "rabbits", "women"]
 
     noun = random.choice(nouns)
     return noun
@@ -44,18 +49,23 @@ def get_noun(quantity):
 
 def get_verb(quantity, tense):
     if tense == "past":
-        verbs = ["drank", "ate", "grew", "laughed", "thought", "ran", "slept", "talked", "walked", "wrote"]
+        verbs = ["drank", "ate", "grew", "laughed", "thought",
+                 "ran", "slept", "talked", "walked", "wrote"]
     elif tense == "present" and quantity == 1:
-        verbs = ["drinks", "eats", "grows", "laughs", "thinks", "runs", "sleeps", "talks", "walks", "writes"]
+        verbs = ["drinks", "eats", "grows", "laughs", "thinks",
+                 "runs", "sleeps", "talks", "walks", "writes"]
     elif tense == "present" and quantity != 1:
-        verbs = ["drink", "eat", "grow", "laugh", "think", "run", "sleep", "talk", "walk", "write"]
+        verbs = ["drink", "eat", "grow", "laugh", "think",
+                 "run", "sleep", "talk", "walk", "write"]
     elif tense == "future":
-        verbs = ["will drink", "will eat", "will grow", "will laugh", "will think", "will run", "will sleep", "will talk", "will walk", "will write"]
+        verbs = ["will drink", "will eat", "will grow", "will laugh", "will think",
+                 "will run", "will sleep", "will talk", "will walk", "will write"]
     else:
         print("Type the correct tense.")
         return None
 
     verb = random.choice(verbs)
     return verb
+
 
 main()
