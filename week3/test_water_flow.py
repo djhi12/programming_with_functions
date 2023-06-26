9f20982701dee277f935d1ba59f10b1db50ee4c8
Daniel John Idorot <ido20002@byui.edu>
2023-06-26 17:49:00 +0800

    Initial commit

--- week3/test_water_flow.py
+++ week3/test_water_flow.py
@@ -1,22 +1,81 @@
-from pytest import approx
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
+import pytest
+from water_flow import (
+    water_column_height,
+    pressure_gain_from_water_height,
+    pressure_loss_from_pipe,
+    pressure_loss_from_fittings,
+    reynolds_number,
+    pressure_loss_from_pipe_reduction
+)
 
 def test_water_column_height():
-    assert water_column_height(0, 0) == approx(0)
-    assert water_column_height(0, 10) == approx(7.5)
-    assert water_column_height(25, 0) == approx(25)
-    assert water_column_height(48.3, 12.8) == approx(57.9)
+    tower_height = 10.0
+    tank_height = 5.0
+    expected_height = 15.75
+    assert water_column_height(tower_height, tank_height) == pytest.approx(expected_height, 0.01)
+
+    tower_height = 15.0
+    tank_height = 8.0
+    expected_height = 19.0
+    assert water_column_height(tower_height, tank_height) == pytest.approx(expected_height, 0.01)
 
 def test_pressure_gain_from_water_height():
-    assert pressure_gain_from_water_height(0) == approx(0)
-    assert pressure_gain_from_water_height(10) == approx(98.82)
-    assert pressure_gain_from_water_height(25) == approx(245.55)
+    height = 20.0
+    expected_pressure = 196.08  # kilopascals
+    assert pressure_gain_from_water_height(height) == pytest.approx(expected_pressure, 0.01)
+
+    height = 25.0
+    expected_pressure = 245.10  # kilopascals
+    assert pressure_gain_from_water_height(height) == pytest.approx(expected_pressure, 0.01)
 
 def test_pressure_loss_from_pipe():
-    assert pressure_loss_from_pipe(0.1, 100, 0.02, 2) == approx(-2.495)
-    assert pressure_loss_from_pipe(0.2, 50, 0.01, 1) == approx(-0.249)
-    assert pressure_loss_from_pipe(0.3, 200, 0.03, 3) == approx(-13.365)
-    assert pressure_loss_from_pipe(0.4, 150, 0.02, 2.5) == approx(-22.372)
-    assert pressure_loss_from_pipe(0.5, 100, 0.02, 1.5) == approx(-3.328)
-    assert pressure_loss_from_pipe(0.6, 50, 0.01, 1.2) == approx(-0.065)
-    assert pressure_loss_from_pipe(0.7, 200, 0.03, 2.8) == approx(-22.620)
+    pipe_diameter = 0.2
+    pipe_length = 50.0
+    friction_factor = 0.02
+    fluid_velocity = 1.5
+    expected_pressure_loss = -0.3075  # kilopascals
+    assert pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity) == pytest.approx(expected_pressure_loss, 0.001)
+
+    pipe_diameter = 0.3
+    pipe_length = 40.0
+    friction_factor = 0.015
+    fluid_velocity = 2.0
+    expected_pressure_loss = -0.432  # kilopascals
+    assert pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity) == pytest.approx(expected_pressure_loss, 0.001)
+
+def test_pressure_loss_from_fittings():
+    fluid_velocity = 1.5
+    quantity_fittings = 3
+    expected_pressure_loss = -0.045  # kilopascals
+    assert pressure_loss_from_fittings(fluid_velocity, quantity_fittings) == pytest.approx(expected_pressure_loss, 0.001)
+
+    fluid_velocity = 2.0
+    quantity_fittings = 4
+    expected_pressure_loss = -0.1067  # kilopascals
+    assert pressure_loss_from_fittings(fluid_velocity, quantity_fittings) == pytest.approx(expected_pressure_loss, 0.001)
+
+def test_reynolds_number():
+    hydraulic_diameter = 0.2
+    fluid_velocity = 1.5
+    expected_reynolds_number = 249833.33
+    assert reynolds_number(hydraulic_diameter, fluid_velocity) == pytest.approx(expected_reynolds_number, 0.01)
+
+    hydraulic_diameter = 0.3
+    fluid_velocity = 2.0
+    expected_reynolds_number = 499666.67
+    assert reynolds_number(hydraulic_diameter, fluid_velocity) == pytest.approx(expected_reynolds_number, 0.01)
+
+def test_pressure_loss_from_pipe_reduction():
+    larger_diameter = 0.2
+    fluid_velocity = 1.5
+    reynolds_number = 100000
+    smaller_diameter = 0.1
+    expected_pressure_loss = -3.744  # kilopascals
+    assert pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter) == pytest.approx(expected_pressure_loss, 0.001)
+
+    larger_diameter = 0.3
+    fluid_velocity = 2.0
+    reynolds_number = 200000
+    smaller_diameter = 0.2
+    expected_pressure_loss = -4.85  # kilopascals
+    assert pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter) == pytest.approx(expected_pressure_loss, 0.001)
--- week3/water_flow.py
+++ week3/water_flow.py
@@ -1,14 +1,28 @@
+GRAVITY = 9.80665  # Earth's acceleration of gravity in m/s^2
+WATER_DENSITY = 998.2  # Density of water in kg/m^3
+WATER_VISCOSITY = 0.0010016  # Dynamic viscosity of water in Pascal seconds
+
 def water_column_height(tower_height, tank_height):
     h = tower_height + 3 * tank_height / 4
     return h
 
 def pressure_gain_from_water_height(height):
-    p = 998.2  # density of water in kg/m^3
-    g = 9.80665  # acceleration due to gravity in m/s^2
-    P = p * g * height / 1000
+    P = WATER_DENSITY * GRAVITY * height / 1000
     return P
 
 def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
-    p = 998.2  # density of water in kg/m^3
-    P = -friction_factor * pipe_length * p * fluid_velocity ** 2 / (2000 * pipe_diameter)
+    P = -friction_factor * pipe_length * WATER_DENSITY * fluid_velocity ** 2 / (2000 * pipe_diameter)
+    return P
+
+def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+    P = -0.04 * WATER_DENSITY * fluid_velocity**2 * quantity_fittings / 2000
+    return P
+
+def reynolds_number(hydraulic_diameter, fluid_velocity):
+    R = WATER_DENSITY * hydraulic_diameter * fluid_velocity / WATER_VISCOSITY
+    return R
+
+def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
+    k = (0.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter)**4 - 1)
+    P = -k * WATER_DENSITY * fluid_velocity**2 / 2000
     return P
